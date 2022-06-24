92eacd6ccca74ea4a52002385d079ca191c60539
Ramón Elías Toro <91712358+RAELTO@users.noreply.github.com>
2022-06-24 00:26:30 -0500

    login initial validations were added

--- js/main.js
+++ js/main.js
@@ -3,11 +3,29 @@
 var app = new Vue({
     el: '#app',
     data: {
-        //data here
+        users: [
+            {id: 1, name: 'test1', lastname: 'lastname1', email: 'test1@example.com', pass: '1234', token: false},
+        ],
+        userinput: '',
+        passinput: '',
+        href: '#',
     },
     methods: {
-        abc(){
-            
+        login(){
+            if (this.userinput.length > 0 && this.passinput.length > 0) {
+                const index = this.users.findIndex((object) => {
+                    return object.email == this.userinput;
+                });
+    
+                if(index != -1 && this.passinput === this.users[index].pass){
+                    this.href = 'app.html'
+                    alert('Valid user!');
+                }else{
+                    alert('Invalid password or email');
+                }
+            }else{
+                alert('An email and password must be provided');
+            }
         }
     },
 });
