a06fec512138a1b7c00b3183ff61b99caba60fc2
Ramón Elías Toro <91712358+RAELTO@users.noreply.github.com>
2022-06-24 01:24:04 -0500

    local storage to save users data and token to be able to use the main app was added

--- js/main.js
+++ js/main.js
@@ -6,9 +6,14 @@ var app = new Vue({
         users: [
             {id: 1, name: 'test1', lastname: 'lastname1', email: 'test1@example.com', pass: '1234', token: false},
         ],
+        newArrUsers: [],
+        pets: [
+            {id: 1, name: 'Pipo', race: 'Creole', age: 1, status: false}
+        ],
         userinput: '',
         passinput: '',
         href: '#',
+        pos: '',
     },
     methods: {
         login(){
@@ -18,7 +23,10 @@ var app = new Vue({
                 });
     
                 if(index != -1 && this.passinput === this.users[index].pass){
+                    this.pos = index;
                     this.href = 'app.html'
+                    this.newArrUsers[index].token = true;
+                    this.updateLocalStorage();
                     alert('Valid user!');
                 }else{
                     alert('Invalid password or email');
@@ -26,6 +34,22 @@ var app = new Vue({
             }else{
                 alert('An email and password must be provided');
             }
-        }
+        },
+        listData(){
+            this.newArrUsers = this.users;
+        },
+        updateLocalStorage(){
+            localStorage.setItem('users', JSON.stringify(this.newArrUsers));
+            localStorage.setItem('pos', JSON.stringify(this.pos));
+        },
     },
+    created(){
+        if (localStorage.getItem('users') !== null) {
+            this.newArrUsers = JSON.parse(localStorage.getItem('users'));
+            this.pos = JSON.parse(localStorage.getItem('pos'));
+        }else{
+            this.listData();
+            this.pos = this.pos
+        }
+    }
 });
