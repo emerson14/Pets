cd1ff09fb790143f3c86a7448217c7d7d475ee9e
Andres Cupa <jamendoza23@misena.edu.co>
2022-06-26 14:52:27 -0500

    alertSmall

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ var app = new Vue({
                 toast: true,
                 position: 'top-center',
                 showConfirmButton: false,
-                timer: 3000,
+                timer: 2000,
                 timerProgressBar: true,
                 didOpen: (toast) => {
                   toast.addEventListener('mouseenter', Swal.stopTimer)
